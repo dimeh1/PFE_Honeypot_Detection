533cd7b4026c8656b618416fb8717819ee9ea23b
dimeh1 <hadimehdi9876@gmail.com>
2026-03-03 16:36:28 +0100

    Correction de la features kernel_latency

--- core/network_scanner.py
+++ core/network_scanner.py
@@ -65,7 +65,7 @@ def get_temporal_features(target_ip, target_port=80, count=5):
 
     features_B = {
         "jitter" : 0.0,
-        "kernel_lantency" : 0.0,
+        "kernel_latency" : 0.0,
         "handshake_delay" : 0.0,
         "latency-ratio" : 0.0
     }
